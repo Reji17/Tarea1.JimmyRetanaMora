a074c4e8ba8c1a3936818a5248a7aa8b4c68944e
Jim <jimmy.retana@gmail.com>
2020-10-13 00:31:01 -0600

    Se crea la estructura del menú y la clase Persona

---
+++ Tarea1.JimmyRetanaMora/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1.JimmyRetanaMora
+{
+    class Persona
+    {
+        string nombre;
+        string primerApellido;
+        string segundoApellido;
+        int identificacion;
+    }
+}
--- Tarea1.JimmyRetanaMora/Program.cs
+++ Tarea1.JimmyRetanaMora/Program.cs
@@ -6,7 +6,40 @@ namespace Tarea1.JimmyRetanaMora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenido al programa");
+            Console.WriteLine("Menú Principal");
+            Console.WriteLine("Seleccione una opción escribiendo el número de la misma y luego presionando Enter");
+            Console.WriteLine("1-Registrar clientes");
+            Console.WriteLine("2-Registrar funcionarios");
+            Console.WriteLine("3-Registrar hidrómetro");
+            Console.WriteLine("4-Registrar categoría de servicios");
+            Console.WriteLine("5-Mostrar todos los registros");
+            Console.WriteLine("6-Salir");
+
+            switch (Console.Read())
+            {
+                case '1':
+                    Console.Write("Registro de clientes");
+                    break;
+                case '2':
+                    Console.Write("Regristro de funcionarios");
+                    break;
+                case '3':
+                    Console.Write("Registro de hidrómetros");
+                    break;
+                case '4':
+                    Console.Write("Registro de categoría de servicios");
+                    break;
+                case '5':
+                    Console.Write("Mostrando todos los registros");
+                    break;
+                case '6':
+                    Console.Write("Finalizando el programa");
+                    Environment.Exit(0);
+                    break;
+            }
+            /*Crear menu*/
+
         }
     }
 }
